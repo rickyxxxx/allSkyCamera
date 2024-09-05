4d988b43507c01d52a6aaf347f6f6faa7594a7c2
Ruitao Xu <rickyxuabc@gmail.com>
2024-09-05 13:34:06 -0700

    getDateTime() test 2

--- c++/SingleFrameMode.cpp
+++ c++/SingleFrameMode.cpp
@@ -439,7 +439,9 @@ int main(int argc, char *argv[])
     printf("GetQHYCCDSingleFrame: %d x %d, bpp: %d, channels: %d, success.\n", roiSizeX, roiSizeY, bpp, channels);
     uint32_t dataSize = 3856 * 2180 * 2;
     printf("%s\n", getDateTime());
-    std::ofstream outFile("output.bin", std::ios::binary);
+    char *filename = strcat(getDateTime(), ".bin");
+    printf("%s\n", filename);
+    std::ofstream outFile("../shared/bin/output.bin", std::ios::binary);
     if (outFile.is_open()) {
         outFile.write(reinterpret_cast<char*>(pImgData), dataSize);
     } else {
