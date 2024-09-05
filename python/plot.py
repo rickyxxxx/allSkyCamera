90c39f856e53c3f6c8572fe3562a25b571a0ae62
Ruitao Xu <rickyxuabc@gmail.com>
2024-09-05 13:20:38 -0700

    switching the image rendering library to PIL

---
+++ python/plot.py
@@ -0,0 +1,12 @@
+import os
+
+import numpy as np
+from PIL import Image
+import tqdm
+
+binary_files = os.listdir("../shared/bin")
+
+for file in tqdm.tqdm(binary_files):
+    data = np.fromfile(f"../shared/bin/{file}", dtype=np.uint16).reshape(2180, 3856)
+    image = Image.fromarray(data)
+    image.save(f"../shared/img/{file.replace('.bin', '.tif')}")
