c24a42215d6589dd0d5221df8e4efc43e7313797
Ruitao Xu <rickyxuabc@gmail.com>
2024-09-11 12:03:25 -0700

    quickfix to CameraFunctions.cpp

--- c++/CameraFunctions.cpp
+++ c++/CameraFunctions.cpp
@@ -4,42 +4,27 @@
 #include <string.h>
 #include "qhyccd.h"
 
-
-std::string SDKVersion();
-
-
 // functions that are to be called from Python
-extern "C"{
+extern "C" {
     std::string SDKVersion();
 }
 
-
-std::string SDKVersion()
-{
-    unsigned int  YMDS[4];
+std::string SDKVersion() {
+    unsigned int YMDS[4];
     unsigned char sVersion[80];
 
-    memset ((char *)sVersion,0x00,sizeof(sVersion));
-    GetQHYCCDSDKVersion(&YMDS[0],&YMDS[1],&YMDS[2],&YMDS[3]);
+    memset(sVersion, 0x00, sizeof(sVersion));
+    GetQHYCCDSDKVersion(&YMDS[0], &YMDS[1], &YMDS[2], &YMDS[3]);
 
-    if ((YMDS[1] < 10)&&(YMDS[2] < 10))
-    {
-        sprintf((char *)sVersion,"V20%d0%d0%d_%d\n",YMDS[0],YMDS[1],YMDS[2],YMDS[3]	);
-    }
-    else if ((YMDS[1] < 10)&&(YMDS[2] > 10))
-    {
-        sprintf((char *)sVersion,"V20%d0%d%d_%d\n",YMDS[0],YMDS[1],YMDS[2],YMDS[3]	);
-    }
-    else if ((YMDS[1] > 10)&&(YMDS[2] < 10))
-    {
-        sprintf((char *)sVersion,"V20%d%d0%d_%d\n",YMDS[0],YMDS[1],YMDS[2],YMDS[3]	);
-    }
-    else
-    {
-        sprintf((char *)sVersion,"V20%d%d%d_%d\n",YMDS[0],YMDS[1],YMDS[2],YMDS[3]	);
+    if ((YMDS[1] < 10) && (YMDS[2] < 10)) {
+        sprintf((char *)sVersion, "V20%d0%d0%d_%d\n", YMDS[0], YMDS[1], YMDS[2], YMDS[3]);
+    } else if ((YMDS[1] < 10) && (YMDS[2] >= 10)) {
+        sprintf((char *)sVersion, "V20%d0%d%d_%d\n", YMDS[0], YMDS[1], YMDS[2], YMDS[3]);
+    } else if ((YMDS[1] >= 10) && (YMDS[2] < 10)) {
+        sprintf((char *)sVersion, "V20%d%d0%d_%d\n", YMDS[0], YMDS[1], YMDS[2], YMDS[3]);
+    } else {
+        sprintf((char *)sVersion, "V20%d%d%d_%d\n", YMDS[0], YMDS[1], YMDS[2], YMDS[3]);
     }
 
     return std::string((char *)sVersion);
 }
-
-
