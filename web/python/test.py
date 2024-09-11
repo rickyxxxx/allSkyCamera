9da150057ce6aebea060488f4da5564f65b8a220
Ruitao Xu <rickyxuabc@gmail.com>
2024-09-11 12:38:35 -0700

    specifying the return type of the SDKVersion() function in python

--- web/python/test.py
+++ web/python/test.py
@@ -3,6 +3,8 @@ import ctypes
 # Load the shared library
 lib = ctypes.CDLL('../../c++/libcamera.so')
 
+lib.SDKVersion.restype = ctypes.c_char_p
+
 sdk_version = lib.SDKVersion()
 
 print(f"Version: {sdk_version}")
