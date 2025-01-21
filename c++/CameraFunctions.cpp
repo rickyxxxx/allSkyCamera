966b2de8a06dc7ada6cd059f6dcb40f1b207e297
Ruitao Xu <rickyxuabc@gmail.com>
2025-01-21 00:59:24 -0800

    fixed getChipInfo method missing problem

--- c++/CameraFunctions.cpp
+++ c++/CameraFunctions.cpp
@@ -33,6 +33,15 @@ void SDKVersion(unsigned int *version) {
     GetQHYCCDSDKVersion(&version[0], &version[1], &version[2], &version[3]);
 }
 
+unsigned int getChipInfo(qhyccd_handle *pCamHandle, unsigned int *scanInfo, double *chipInfo) {
+    // get chip info
+    unsigned int retVal = GetQHYCCDChipInfo(pCamHandle, &chipInfo[0], &chipInfo[1], &scanInfo[0], &scanInfo[1],
+                                            &chipInfo[2], &chipInfo[3], &scanInfo[2]);
+    if (QHYCCD_SUCCESS != retVal)
+        return 1;       // error getting the camera's chip info
+
+    return 0;
+}
 
 unsigned int getCameraId(char *camId) {
     // init SDK
