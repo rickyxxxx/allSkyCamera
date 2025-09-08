d88db7d9c4e71561dbe72e9f5317169088731697
ricky <rickyxuabc@gmail.com>
2025-09-08 14:17:32 -0700

    adding methods to read temperature and humidity as str in AHT20.py. Resolved the issue of GPS module blocking the initialization of the camera in GTU7.py

--- camera/gps_driver/GTU7.py
+++ camera/gps_driver/GTU7.py
@@ -1,124 +1,112 @@
+import threading
 import serial
 import serial.tools.list_ports as lp
-from time import time, sleep
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from time import sleep
 from typing import Optional
 
-from timezonefinder import TimezoneFinder
 from micropyGPS import MicropyGPS as GPSDecoder
 
 
-Coord = list[int, float, str]
-
-
 class GTU7:
-    def __init__(self, refresh_interval=2, timeout=20000) -> None:
-        device = self.__get_device()
+    def __init__(self) -> None:
+        self.status = "initializing gps device..."
 
-        self.timeout = timeout
-        self.refresh_interval = refresh_interval
+        if (device := self.__get_device()) is None:
+            return      # abort gps initialization if not connected
 
-        self.last_read_time = time()
-        self.gps_decoder = GPSDecoder()
-        self.timezone_finder = TimezoneFinder()
-        self.ser = serial.Serial(device, 9600, timeout=1)
+        self._gps_decoder = GPSDecoder()
 
-        self.ser.readline()     # Skip the first line
-        self.__wait_for_connection()
+        self._ser = serial.Serial(device, 9600, timeout=1)
+        self._ser.readline()     # Skip the first line
 
-    @staticmethod
-    def __get_device() -> str:
-        # search for the GPS device
+        self._terminate = threading.Event()
+        self._t = threading.Thread(target=self.main_loop)
+        self._t.start()
+
+        # self.timezone_finder = TimezoneFinder()
+
+    def __get_device(self) -> Optional[str]:
+        # establish serial connection between raspberry pi and the GPS module
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if "GPS" not in port.description:
                 continue
+            self.status = "GPS device initialized"
             return port.device
-        raise RuntimeError("No GPS device found")
+        self.status = "GPS device not available!"
+
+    def __connect_device(self) -> None:
+        # connect to satellites for GPS readings
+        while not self._terminate.is_set():
+            self.update()
+            # satellites currently in use, need 4 to get location
+            if self._gps_decoder.satellites_in_use >= 4:
+                return
+            self.status = f"Searching for GPS signals; Satellites in view/used: {self.sat_in_view}/{self.sat_in_use}"
+            sleep(1)
+
+    @property
+    def sat_in_view(self) -> str:
+        return self._gps_decoder.satellites_in_view
+
+    @property
+    def sat_in_use(self) -> str:
+        return self._gps_decoder.satellites_in_use
 
     def update(self) -> None:
         # update the GPS data from the device
-        sentence = self.ser.readline().decode("utf-8")
+        sentence = self._ser.readline().decode("utf-8")
 
         for word in sentence:
-            self.gps_decoder.update(word)
+            self._gps_decoder.update(word)
 
-        self.last_read_time = time()
-
-    def __wait_for_connection(self) -> None:
-        sat_in_use = 0      # need a minimum of 4 satellites to get a correct location
-        for i in range(self.timeout):
-            self.update()
-            print(self.gps_decoder.satellites_in_use, self.gps_decoder.satellites_in_view)
-            sat_in_use = self.gps_decoder.satellites_in_use
-            if sat_in_use >= 4:
-                return
-            sleep(0.5)
-
-        # timeout reached
-        if sat_in_use:
-            raise TimeoutError(f"Not enough satellites: {sat_in_use} were in use "
-                               f"but a minimum of 4 are required.")
-        else:
-            raise TimeoutError("No satellites were connected")
-
-    def get_location(self) -> Optional[tuple[Coord, Coord]]:
-        if time() - self.last_read_time > self.refresh_interval:
-            self.update()
-        gps = self.gps_decoder
-
-        latitude = gps.latitude
-        longitude = gps.longitude
+    def update_location(self) -> None:
+        gps = self._gps_decoder
 
         if not (1 <= gps.hdop < 50 and 1 <= gps.vdop < 50 and 1 <= gps.pdop < 50):
-            return None     # if the uncertainty is too high or too low, reject the data
+            return      # discard result if uncertainty is too high or too low
 
-        return latitude, longitude
+        lat, lng = gps.latitude, gps.longitude      # dtype: list(int, float, str)
+        lat_m, lng_m = int(lat[1]), int(lng[1])
+        lat_s, lng_s = (lat[1] - lat_m) * 60, (lng[1] - lng_m) * 60
 
-    def get_location_str(self) -> str:
-        coord = self.get_location()
-        if coord is None:
-            return "Location Not Available"
-        lat, lng = coord
-        lat_min = int(lat[1])
-        lat_sec = (lat[1] - lat_min) * 60
-        lng_min = int(lng[1])
-        lng_sec = (lng[1] - lng_min) * 60
-        return f"{lat[0]}°{lat_min}'{lat_sec:.2f}\"{lat[2]}  {lng[0]}°{lng_min}'{lng_sec:.2f}\"{lng[2]}"
+        self.status = f"{lat[0]}°{lat_m}'{lat_s:.2f}\"{lat[2]} {lng[0]}°{lng_m}'{lng_s:.2f}\"{lng[2]}"
 
-    def get_timezone(self) -> Optional[str]:
-        ctr = 0
-        while (coord := self.get_location()) is None:
-            if ctr >= self.timeout:
-                return
-            ctr += 1
-        lat, lng = coord
-        lat = (lat[0] + lat[1] / 60) * (-1 if lat[2] == "S" else 1)
-        lng = (lng[0] + lng[1] / 60) * (-1 if lng[2] == "W" else 1)
-        return self.timezone_finder.timezone_at(lng=lng, lat=lat)
+    def main_loop(self) -> None:
+        self.__connect_device()
 
-    def get_datetime(self) -> str:
-        dt = datetime.now()
-        if timezone := self.get_timezone():
-            dt = dt.astimezone(ZoneInfo(timezone))
-        return str(dt)
+        while not self._terminate.is_set():
+            self.update()
+            self.update_location()
+            sleep(300)
 
     def close(self) -> None:
-        if self.ser is None:
+        self._terminate.set()
+        self._t.join()
+        if self._ser is None:
             return
-        self.ser.close()
+        self._ser.close()
+
+    # def get_timezone(self) -> Optional[str]:
+    #     ctr = 0
+    #     while (coord := self.get_location()) is None:
+    #         if ctr >= self.timeout:
+    #             return
+    #         ctr += 1
+    #     lat, lng = coord
+    #     lat = (lat[0] + lat[1] / 60) * (-1 if lat[2] == "S" else 1)
+    #     lng = (lng[0] + lng[1] / 60) * (-1 if lng[2] == "W" else 1)
+    #     return self.timezone_finder.timezone_at(lng=lng, lat=lat)
+    #
+    # def get_datetime(self) -> str:
+    #     dt = datetime.now()
+    #     if timezone := self.get_timezone():
+    #         dt = dt.astimezone(ZoneInfo(timezone))
+    #     return str(dt)
 
 
 if __name__ == "__main__":
-    from time import sleep
     gt_u7 = GTU7()
     while True:
-        try:
-            print(gt_u7.get_location_str())
-            print(gt_u7.get_datetime())
-            print()
-            sleep(2)
-        except KeyboardInterrupt:
-            gt_u7.close()
-
+        print(f'\r{gt_u7.status}', end='')
+        sleep(0.5)
--- camera/theraml_driver/AHT20.py
+++ camera/theraml_driver/AHT20.py
@@ -1,24 +1,28 @@
-import time
 import board
 import adafruit_ahtx0
 
 
 class AHT20:
-    def __init__(self):
+    def __init__(self) -> None:
         i2c = board.I2C()  # uses board.SCL and board.SDA
-        self.sensor = adafruit_ahtx0.AHTx0(i2c)
+        self._sensor = adafruit_ahtx0.AHTx0(i2c)
 
-    def get_temperature(self):
-        return self.sensor.temperature
+    @property
+    def temp(self) -> str:
+        return f"{self._sensor.temperature:.1f}°C"
 
-    def get_humidity(self):
-        return self.sensor.relative_humidity
+    @property
+    def humidity(self) -> str:
+        return f"{self._sensor.relative_humidity:.1f}%"
+
+    def temp_f(self) -> float:
+        return self._sensor.temperature
+
+    def humidity_f(self) -> float:
+        return self._sensor.temperature
+
 
 if __name__ == "__main__":
-    a = AHT20()
+    sensor = AHT20()
     while True:
-        temp = a.get_temperature()
-        hum = a.get_humidity()
-        print(f"temperature: {temp}     humidity: {hum}")
-        print(type(temp), type(hum))
-        time.sleep(5)
+        print(sensor.temp, sensor.humidity)
