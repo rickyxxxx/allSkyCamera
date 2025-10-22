3b001d0099001f450b8efb60dc1dfb0050fcacd6
ricky <rickyxuabc@gmail.com>
2025-10-22 15:06:34 -0700

    adding logging to thermal.py

--- src/common/thermal.py
+++ src/common/thermal.py
@@ -1,14 +1,22 @@
+import logging
+
 import board
 import adafruit_ahtx0
 
 
 class AHT20:
     def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+
         i2c = board.I2C()  # uses board.SCL and board.SDA
         self._sensor = adafruit_ahtx0.AHTx0(i2c)
+        self.logger.info("Thermal Sensor Initialized")
 
-        ret = self._sensor.calibrate()
-        print(f"Thermal Sensor Calibrated: {ret}")
+        calibrated = self._sensor.calibrate()
+        if calibrated:
+            self.logger.info("Thermal Sensor Calibrated")
+        else:
+            self.logger.warn("Thermal Sensor Calibration Failed, Readings May Be Inaccurate")
 
     @property
     def temp(self) -> str:
@@ -18,9 +26,11 @@ class AHT20:
     def humidity(self) -> str:
         return f"{self._sensor.relative_humidity:.1f}%"
 
+    @property
     def temp_f(self) -> float:
         return self._sensor.temperature
 
+    @property
     def humidity_f(self) -> float:
         return self._sensor.temperature
 
